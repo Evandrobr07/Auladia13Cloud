c4675e286cbad76f87539ab6945667b23a268dd4
Evandrobr07 <evandrobrfilho@gmail.com>
2026-05-13 21:15:42 -0300

    ajuste no layout do cabeçalho

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ const Counter: React.FC = () => {
 
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
-      <h1 className="text-4xl mb-4">Contador Simples</h1>
+      <h1 className="text-4xl mb-4">Contador Simples teste do novo commit</h1>
       <div className="flex items-center justify-center space-x-4 bg-white p-4 rounded-2xl shadow-lg">
         <button
           className="px-4 py-2 bg-red-500 text-white rounded-lg hover:bg-red-600"
